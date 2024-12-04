3af7ceb07bc5e4e51d5656766da7f7e0329b6da6
The-Parthib <rijupanja81@gmail.com>
2024-12-04 21:16:36 +0530

    footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       </main>
       <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 text-center text-gray-400">
-          <p>Made with 🤩 by The-Parthib</p>
+          <p>Made by The-Parthib</p>
         </div>
       </footer>
     </div>
